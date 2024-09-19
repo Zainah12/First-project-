d4d54ea39fbfa672b5f0f4e3bb984a094453ee9a
Zainah12 <nakanjakozainah333@gmail.com>
2024-09-19 18:16:13 +0300

    Zainah.php

--- Zainah.php
+++ Zainah.php
@@ -1,5 +1,19 @@
 <?php
-$myname="zainah";
-echo"myname is:".$myname;
+$Myname = "Nakanjako Zainah";
+echo "Name: " . $Myname . "\n";
 
+$DOB = "25/05/2003";
+echo "Date of birth: " . $DOB . "\n";
+
+$Home = "Makindy-kampala";
+echo "Home Address: " . $Home . "\n";
+
+$Welcome = "glad to see you home";
+echo "We are " . $Welcome . " Zainah \n";
+
+$Current = 2024;
+$Previous = 2003;
+$Age = $Current - $Previous;
+
+echo "You are now " . $Age . " years old ";
 ?>
